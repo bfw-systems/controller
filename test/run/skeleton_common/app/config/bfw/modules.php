510c30be9053d7af40aa6f3fd6858d7ad53126e4
bulton-fr <bulton.fr@gmail.com>
2018-07-24 16:17:01 +0200

    Test run : update bfw config filename

--- test/run/skeleton_common/app/config/bfw/modules.php
+++ test/run/skeleton_common/app/config/bfw/modules.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = require(__DIR__.'/config.php.original');
+$config = require(__DIR__.'/modules.php.original');
 
 $config['modules']['controller']['name']    = 'bfw-controller';
 $config['modules']['controller']['enabled'] = true;
